cea5f04d0f7bb52b982f24abff1d689d1afbf079
outan <outanwang@gmail.com>
2017-04-24 01:25:38 +0900

    add web api client to node_slack_sdk_test.js

--- node_slack_sdk_test.js
+++ node_slack_sdk_test.js
@@ -13,3 +13,16 @@ webhook.send(message, function(err, res) {
         console.log('Message sent: ', res);
     }
 });
+
+var WebClient = require('@slack/client').WebClient;
+
+var token = process.env.SLACK_API_TOKEN || ''; //see section above on sensitive data
+
+var web = new WebClient(token);
+web.chat.postMessage('bot_test', 'from webClient,hello', function(err, res) {
+    if (err) {
+        console.log('Error:', err);
+    } else {
+        console.log('Message sent: ', res);
+    }
+});
